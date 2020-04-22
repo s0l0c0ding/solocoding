c184b088a112131839bdd2e4e2476075799d4720
s0l0c0ding <>
2020-04-22 21:10:55 +0200

    about moudle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
   { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
 ];
 
 @NgModule({
