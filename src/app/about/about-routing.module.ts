2971c1edf9cf4da37fddd861bb4450eec4e15583
s0l0c0ding <>
2020-04-25 13:35:20 +0200

    seo tags

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AboutComponent } from './about.component';
 
-const routes: Routes = [{ path: '', component: AboutComponent }];
+const routes: Routes = [{ path: '', component: AboutComponent },
+{ path: 'about', component: AboutComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
