0c4284c1c653ca216db9de58b93205d1e21ed7f1
s0l0c0ding <>
2020-05-25 20:59:03 +0200

    [sharing] title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
-  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
-  { path: 'portfolio', loadChildren: () => import('./portfolio/portfolio.module').then(m => m.PortfolioModule) },
+  { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule), data:{'title': "Blog"} },
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule), data:{'title': "soloCoding"} },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule), data:{'title': "About me"} },
+  { path: 'portfolio', loadChildren: () => import('./portfolio/portfolio.module').then(m => m.PortfolioModule), data:{'title': "Portfolio"} },
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
-
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { ScullyRoutesService } from '@scullyio/ng-lib';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'soloCoding';
+
+  postTitle: string;
+
+  public constructor(private titleService: Title,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private scully: ScullyRoutesService) {
+
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map(() => this.activatedRoute),
+      map(route => {
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route;
+      }),
+      filter(route => route.outlet === 'primary')
+    ).subscribe((route: ActivatedRoute) => {
+      this.scully.getCurrent().pipe(
+        map(current => current.title )
+      ).subscribe(
+        title => this.postTitle= title
+      );
+      console.log(this.postTitle);
+      
+      if(this.postTitle){
+        this.titleService.setTitle(this.postTitle);
+        
+      } else{
+      this.titleService.setTitle(route.snapshot.data['title']);
+    }
+    });
+  }
+
 }
