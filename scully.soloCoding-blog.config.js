6f0bf59cef5ae408cda6880ed536c8016cd3f88e
s0l0c0ding <>
2020-05-28 23:52:34 +0200

    [scully] plugin disable angular

--- scully.soloCoding-blog.config.js
+++ scully.soloCoding-blog.config.js
@@ -4,9 +4,12 @@ var scully_1 = require("@scullyio/scully");
 require("./plugins/postCategoryPlugin.js");
 var pluginRepository_1 = require("@scullyio/scully/lib/pluginManagement/pluginRepository");
 scully_1.setPluginConfig('md', pluginRepository_1.pluginTypes[2], { enableSyntaxHighlighting: true });
+var DisableAngular = require('scully-plugin-disable-angular').DisableAngular;
+var postRenderers = [DisableAngular];
 exports.config = {
     projectName: 'soloCoding-blog',
     outDir: './dist/static',
+    defaultPostRenderers: postRenderers,
     routes: {
         '/blog/:slug': {
             type: 'contentFolder',
