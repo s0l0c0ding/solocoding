7870fb42a8978bc94d9b7ff0c17a1ad4bfcd937b
s0l0c0ding <>
2021-08-03 16:58:44 +0200

    :hammer: [dashboard] add quarkus link

--- plugins/postCategoryPlugin.js
+++ plugins/postCategoryPlugin.js
@@ -5,10 +5,12 @@ const postCategoryPlugin = async (route, config = {}) => {
     {route: '/posts/devops'},
     {route: '/posts/spring'},
     {route: '/posts/angular'},
+    {route: '/posts/quarkus'},
     {route: '/posts/it'},
     {route: '/posts/it_devops'},
     {route: '/posts/it_spring'},
-    {route: '/posts/it_angular'}
+    {route: '/posts/it_angular'},
+    {route: '/posts/it_quarkus'}
   ]);
 }
 
