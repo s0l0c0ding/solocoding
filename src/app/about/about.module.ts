dfb164c2e583b9912eba2d24462c2b47c40d80de
bassem <solocoding@proton.me>
2022-06-19 09:50:25 +0200

    :trophy:[feature] donate comp

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
 import { ContactComponent } from '../contact/contact.component';
+import { DonateComponent } from '../donate/donate.component';
 
 
 @NgModule({
-  declarations: [AboutComponent, ContactComponent],
+  declarations: [AboutComponent, ContactComponent, DonateComponent],
   imports: [
     CommonModule,
     AboutRoutingModule
